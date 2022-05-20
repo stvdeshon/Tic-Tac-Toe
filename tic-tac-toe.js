7d94c14b48259fc7f385078a97ea7526112d4437
stvdeshon <93623464+stvdeshon@users.noreply.github.com>
2022-05-20 14:24:29 -0600

    Improve the UI and populate the array properly

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,47 +1,75 @@
-const gameBoard = (() => {
-
-    const board = ['','','','','','','','',''];
-    
-    function turns() {
-        //this conditional actually works
-        const stv = FactoryPlayer('stv');
-
-        if (3 === 3) {
-
-            stv.selectCell();
-        }
-    }
-    return {turns};
-})();
 
 const FactoryPlayer = (name) => {
 
+    const getName = () => name;
     
-    const getName = () => name; //console.log(name);
+    return {getName};
+}
 
-    const selectCell = () => {
-        const cell = document.querySelectorAll('.cells');
+const gameBoard = (() => {
+
+    const board = ['','','','','','','','',''];
+    const cell = document.querySelectorAll('.cells');
+
+
+    function selection() {
+        let playerOneTurn = true;
+
         const marker = (e) => {
-            e.target.style.backgroundImage = 'url(x.png)';
+            const target = e.target;
+            const parent = target.parentNode;
+            const index = [].indexOf.call(parent.children, target);
+            if (playerOneTurn && e.target.style.backgroundImage === '') {
+                e.target.style.backgroundImage = 'url(x.png)' 
+                playerOneTurn = false;
+                board.splice(index, 1, 'x');
 
-            // some condition -> cell.style.backgroundImage = 'url(o.png)';
+                console.log(playerOneTurn);
+                console.log(index);
+                console.log(board);
+                return;
+            }
+            if (!playerOneTurn && e.target.style.backgroundImage === '') {
+                e.target.style.backgroundImage = 'url(o.png)' 
+                playerOneTurn = true;
+                board.splice(index, 1, 'o');
+
+                console.log(playerOneTurn);
+                console.log(index);
+                console.log(board);
+                return;
+            }
         }
         cell.forEach((square) => {
-            square.addEventListener('click', marker);
+                square.addEventListener('click', marker);
         })
     }
 
-    return {getName, selectCell};
-}
+    //create(), winner(), and reset() will need access to these
+    const modal = document.querySelector('#modal');
+    const submit = document.querySelector('#submit');
+    //these are the inputs for players one and two
+    const first = document.getElementById('first');
+    const second = document.getElementById('second');
+    //this displays the vs. message above the board
+    const vs = document.querySelector('#vs');
+    function createPlayer() {
+      
+        submit.addEventListener('click', () => {
+        playerOne =  FactoryPlayer(first.value || 'You');
+        playerTwo = FactoryPlayer(second.value || 'Computer');  
+        modal.classList.add('close');
+        vs.textContent = `${playerOne.getName()} VS ${playerTwo.getName()}`
+        console.log(playerOne.getName());
+        });
+    }
 
-// const gameFlow = (() => {
+    function winner() {
+        //display winner popup and offer reset button
+    }
 
-//  winner = () => {
-    
-    // }
+    createPlayer();
+    selection();
 
-//     return {};
-// })();
-
-gameBoard.turns();
-
+    return {};
+})();
